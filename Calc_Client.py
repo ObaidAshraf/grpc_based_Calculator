da61951493e72c3fe905a82f789106ee907b8e14
ObaidAshraf <ubaidashraf91@gmail.com>
2018-08-25 15:17:25 +0500

    Added Server and Client code

---
+++ Calc_Client.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+import grpc
+
+import Calc_pb2
+import Calc_pb2_grpc
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = Calc_pb2_grpc.CalculatorStub(channel)
+        response = stub.Add(Calc_pb2.AddRequest(n1 = 20, n2 = 10))
+        print(response.n1)
+
+
+if __name__ == '__main__':
+    run()
--- Calc_Server.py
+++ Calc_Server.py
@@ -27,3 +27,13 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     Calc_pb2_grpc.add_CalculatorServicer_to_server(Calculator(), server)
     server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    serve()
